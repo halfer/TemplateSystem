f076d875703269db43d68f4193da6c8fd8ee329f
Jon Hinks <projects.ws@jondh.me.uk>
2013-07-07 20:05:18 +0100

    Can't do type hinting any more, use a subclass test instead

--- ComponentBase.php
+++ ComponentBase.php
@@ -20,6 +20,10 @@ if (!class_exists('TemplateSystem\Change2\ComponentBase'))
 		public function __construct($controller, $root)
 		{
 			// Check controller is of the right type here
+			if (!is_subclass_of($controller, 'TemplateSystem\Change2\ControllerBase'))
+			{
+				throw new Exception('The controller passed to the component must inherit ControllerBase');
+			}
 
 			$this->controller = $controller;
 			$this->root = $root;
